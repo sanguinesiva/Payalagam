96342d824c072e6814da32c0877fb36289f9bc9a
ɐʌᴉS <sivakandaswamy@outlook.com>
2023-01-22 20:48:48 -0600

    constructor

---
+++ src/work/Book.java
@@ -0,0 +1,38 @@
+package work;
+
+public class Book {
+	int price;
+	int discount;
+
+	Book(int price)
+
+	{
+		this.price = price;
+		this.discount = discount;
+
+	}
+
+	public Book(int price, int discount) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		Book Book1 = new Book(50);
+		Book Book2 = new Book(30);
+		Book Book3 = new Book(1000, 500);
+		Book book4 = new Book();
+		Book1.sell();
+		Book3.sell();
+//		System.out.println(this.price);
+
+	}
+
+	void sell() {
+		int price = 45;
+		System.out.println(price);
+		System.out.println(this.price); // dont use this key word inside the static method.
+		System.out.println("discount is " + this.discount);
+
+	}
+
+}
--- src/work/SuperMarket.java
+++ src/work/SuperMarket.java
@@ -1,51 +1,46 @@
 package work;
 
 public class SuperMarket {
-	static String shop_name= "waalmart";
+	static String shop_name = "walmart";
 	String product_name;
 	int product_price;
 	boolean expired;
 	float discount;
-	
+
 	public SuperMarket(String product_name, int product_price) {
 		// TODO Auto-generated constructor stub
 //	System.out.println("siva");
 		this.product_name = product_name;
-		this.product_price = product_price; 
-		// this is used to initialize non static variables. 
+		this.product_price = product_price;
+		// this is used to initialize non static variables.
 		// it is same as class name
 		// it has no return datatype.
-		
-		
+
 	}
-	
-	
+
 	public static void main(String[] args) {
-	
-		SuperMarket product1= new SuperMarket("biscuit ",30);
-		SuperMarket product2= new SuperMarket("bread ",10);
+
+		SuperMarket product1 = new SuperMarket("biscuit ", 30);
+		SuperMarket product2 = new SuperMarket("bread ", 10);
 		product1.sell();
 		product2.pack();
 		System.out.println(product1.expired);
 		System.out.println(product2.discount);
-		
+
 //		product1.product_name= "biscuit";
 //		product2.product_price=30;
 	}
-	
-
 
 	private void pack() {
 		// TODO Auto-generated method stub
-		System.out.println("selling "+ product_name+ product_price);
-		
-	}
+		System.out.println("selling " + product_name + product_price);
 
+	}
 
 	private void sell() {
 		// TODO Auto-generated method stub
-		System.out.println("packing "+ product_name + product_price);
-		
+		System.out.println("packing " + product_name + product_price);
+
 	}
 
 }
