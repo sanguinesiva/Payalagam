cc5283120c72795ba44ab671875e387e31ab14e2
ɐʌᴉS <sivakandaswamy@outlook.com>
2023-01-25 20:39:22 -0600

    assignment

---
+++ src/assignment/School.java
@@ -0,0 +1,26 @@
+package assignment;
+
+public class School extends Theater {
+	
+	int mark;
+	private int salary;
+	static String school_name= "Abc primary school";
+	
+
+		
+		
+	
+	void conduct_exam() {
+		System.out.println("results");
+		
+	}
+	void publsh_result(int mark)
+	{
+	System.out.println("print mark");	
+	
+	}
+	
+	
+}
+
+
---
+++ src/assignment/Teacher.java
@@ -0,0 +1,18 @@
+package assignment;
+
+
+
+public class Teacher {
+	public static void main(String[] args) {
+		
+		School teacher = new School();
+		teacher.conduct_exam();
+		teacher.publsh_result(75);
+		System.out.println("the school name is "+ School.school_name);
+//		System.out.println("teacher salary is  "+ teacher.salary );
+		
+
+		
+	}
+
+}
